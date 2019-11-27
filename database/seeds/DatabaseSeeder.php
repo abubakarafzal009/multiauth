99aff193d4232f8e814f44133b066e98355709b2
unknown <abubakar.ranglerz@gmail.com>
2019-11-27 15:31:27 +0000

    seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,8 +9,37 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+    // public function run()
+    // {
+        
+    // }
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'edit articles']);
+        Permission::create(['name' => 'delete articles']);
+        Permission::create(['name' => 'publish articles']);
+        Permission::create(['name' => 'unpublish articles']);
+        Permission::create(['name' => 'Read articles']);
+
+
+        // create roles and assign created permissions
+
+        // this can be done as separate statements
+        // $role = Role::create(['name' => 'writer']);
+        // $role->givePermissionTo('edit articles');
+
+        // or may be done by chaining
+        $role = Role::create(['name' => 'super-Admin'])
+            ->givePermissionTo(['publish articles', 'unpublish articles','delete articles','edit articles']);
+
+        $role = Role::create(['name' => 'Admin'])
+           ->givePermissionTo(['Read articles']);;
+        $role->givePermissionTo(Permission::all());
+       $user = User::create([ 'name' => 'John', 'email' => 'admn@example.com', 'password' => bcrypt('password'), 'created_at' => '2019-05-28 04:45:27', 'updated_at' => '2019-05-28 04:45:27' ]); 
+       $user->assignRole('Admin');
+$user1 = User::create([ 'name' => 'John Doe', 'email' => 'superadmin@example.com', 'password' => bcrypt('password'), 'created_at' => '2019-05-28 04:45:27', 'updated_at' => '2019-05-28 04:45:27' ]); $user->assignRole('super-Admin');
     }
 }
